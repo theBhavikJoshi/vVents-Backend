69dab5fd2a7957ec67a3bd5a7b0d4f5292e90669
Bhavik Joshi <bhavikjoshi29@gmail.com>
2018-02-26 05:29:48 +0530

    Added CORS

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 mongoose.connect( process.env.MONGODB_URI || keys.MONGOURI);
 
 app.get('/', (req, res) => {
